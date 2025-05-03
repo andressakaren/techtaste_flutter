04d37042213413429e152c3dc23338412f32e03e
Andressa Karen <lqsandressa@gmail.com>
2025-05-03 11:29:33 -0300

    aula 1

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,8 @@
+// condensar as cores
+
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFffa559);
+}
